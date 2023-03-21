bd10db6b2371eafa8eaf354799c1d7c7eeb50c89
Jennifer N-B <JenniferBarker1o1@gmail.com>
2023-03-21 15:36:33 +0000

    37 change profile pic input functional

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="container-fluid">
@@ -30,12 +31,12 @@
                             
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10 mb-3">
-                                    <input name="profile_image" class="form-control" type="file" id="profile_image">
+                                    <input name="profile_image" class="form-control" type="file" id="image">
                                 </div>
                             
                                 <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10 mb- mt-3">
-                                    <img class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                                    <img id="showImage" class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
                                 </div>
                             </div>
                             <input type="submit" value="Update Profile" class="btn btn-info waves-effect waves-light">
@@ -46,5 +47,19 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+    $('#image').change(function(e){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#showImage').attr('src',e.target.result);
+        }
+        reader.readAsDataURL(e.target.files['0']);
+    });
+});
+
+</script>
 
 @endsection
