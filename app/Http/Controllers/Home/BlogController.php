3929573d44c6ecc546dad50abffc0b48ad82ef26
Jennifer N-B <Jenniebarker1o1@gmail.com>
2023-04-06 12:20:45 +0100

    97

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -131,7 +131,7 @@ class BlogController extends Controller
     }// End Method
 
     public function homeBlog() {
-        $allblogs = Blog::latest()->get();
+        $allblogs = Blog::latest()->paginate(3);
         $categories = BlogCategory::orderBy('blog_category','ASC')->get();
         return view('frontend.blog',compact('allblogs','categories'));
     }// End Method
